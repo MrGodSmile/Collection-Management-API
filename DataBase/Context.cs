ec2fedcd2ad82fc8204d9a40c647ddf868385f1b
MrGodSmile <mrgodsmile@yandex.ru>
2022-06-09 22:04:26 +0300

    DBUpdate

---
+++ DataBase/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBase;
+
+public class Context : DbContext
+{
+   public Context(DbContextOptions<Context> options) :
+       base(options)
+   {
+        
+   }   
+   public DbSet<Example> Examples { get; set; }
+}
